66cddb85cdc46d1c1a7d76bf25fb592e446af801
Daniel Ezet <ezetdaniel@gmail.com>
2024-03-23 12:59:10 +0100

    added live stream

---
+++ src/app/play/page.jsx
@@ -0,0 +1,22 @@
+import Bets from "@/components/Bets";
+import Header from "@/components/Header";
+import Live from "@/components/Live";
+import MobileNav from "@/components/MobileNav";
+import Nav from "@/components/Nav";
+
+export default function LivePlay() {
+    return (
+        <>
+            <MobileNav/>
+            <section id="heading">
+                <Header page="/play"/>
+            </section>
+            <section id="navigation">
+                <Nav value="play"/>
+            </section>
+            <section id="_bets">
+                <Live/>
+            </section>
+        </>
+    )
+}
---
+++ src/components/Live.jsx
@@ -0,0 +1,17 @@
+"use client"
+import ReactTwitchEmbedVideo from "react-twitch-embed-video"
+
+const Live = () => {
+  return (
+     <div id="play" className="bg-[#0052FE] p-2 sm:p-8 h-screen sm:h-full">
+            <div className="sm:rounded-lg sm:bg-[#0F212E] sm:border sm:border-[#8D969C] sm:px-16 sm:py-4">
+                <h1 className="p-2 sm:p-8 text-center text-white font-black text-xl sm:text-6xl">LIVE STREAM</h1>
+                <div className="px-1 py-2 sm:px-4 sm:py-4">
+                    <ReactTwitchEmbedVideo channel="hamsgolive" layout={"video-with-chat"} width={"100%"} height={"600px"}/>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Live
